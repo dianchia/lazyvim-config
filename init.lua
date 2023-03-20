7f6243ee41618fc96b9fbb93cda4fa145b47a9ed
dianchia <dianrui520@hotmail.com>
2023-03-20 22:22:53 +0800

    Modified init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.g.python3_host_prog = "C:/Users/ruidian/AppData/Local/nvim/venv/Scripts/python.exe"
+vim.g.python3_host_prog = os.getenv("LOCALAPPDATA") .. "/nvim/venv/Scripts/python.exe"
 require("starry.functions").change_style("dracula_blood")
