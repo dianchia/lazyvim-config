29e13fe143c6f3a6ad7c4b88682b3a23332d9f4e
dianchia <dianrui520@hotmail.com>
2023-04-11 23:30:57 +0800

    Switched to using poetry and added window maximizer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,7 +9,13 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear Search" })
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
 
-keymap.set("n", "<leader>ft", function() util.float_term("pwsh.exe", nil) end, {desc = "Terminal (cwd)"})
+keymap.set("n", "<leader>ft", function()
+  util.float_term("pwsh.exe", nil)
+end, { desc = "Terminal (cwd)" })
+
+-- Windows
+keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make window equal width" })
+keymap.set("n", "<leader>wm", ":MaximizerToggle<CR>", { desc = "Toggle maximize" })
 
 -- Map Ctrl+space to Copilot accept suggestions
 -- keymap.set("i", "<C-space>", 'copilot#accept("<C-space>")', { expr = true, noremap = true })
