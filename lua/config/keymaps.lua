81bc10370e445f8dd5173292b1b927f2cf2e9c0d
dianchia <dianrui520@hotmail.com>
2023-08-30 18:32:10 +0800

    Make vim-maximizer lazy loaded

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,7 +15,6 @@ end, { desc = "Terminal (cwd)" })
 
 -- Windows
 keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make window equal width" })
-keymap.set("n", "<leader>wm", ":MaximizerToggle<CR>", { desc = "Toggle maximize" })
 
 -- Map Ctrl+space to Copilot accept suggestions
 -- keymap.set("i", "<C-space>", 'copilot#accept("<C-space>")', { expr = true, noremap = true })
--- lua/plugins/maximize.lua
+++ lua/plugins/maximize.lua
@@ -1,3 +1,6 @@
 return {
   "szw/vim-maximizer",
+  keys = {
+    { "<leader>wm", "<cmd>MaximizerToggle<CR>", { desc = "Maximize/Minimize a split" } },
+  },
 }
