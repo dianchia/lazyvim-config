3d828bf29993ddfd569255fa1168f3d825bcdec5
dianchia <dianrui520@hotmail.com>
2023-10-01 16:19:08 +0800

    Changed from null-ls to none-ls

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,8 +1,9 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+    -- bootstrap lazy.nvim
+    -- stylua: ignore
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+        lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -10,15 +11,16 @@ require("lazy").setup({
     spec = {
         -- add LazyVim and import its plugins
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-        -- import any extras modules here
-        { import = "lazyvim.plugins.extras.ui.mini-animate" },
-        { import = "lazyvim.plugins.extras.coding.copilot" },
+        -- Formatting
         { import = "lazyvim.plugins.extras.formatting.prettier" },
+        { import = "lazyvim.plugins.extras.formatting.conform" },
         -- Language Support
         { import = "lazyvim.plugins.extras.lang.json" },
         { import = "lazyvim.plugins.extras.lang.python" },
-        -- Something
+        -- Misc
         { import = "lazyvim.plugins.extras.editor.leap" },
+        { import = "lazyvim.plugins.extras.ui.mini-animate" },
+        { import = "lazyvim.plugins.extras.coding.copilot" },
         -- import/override with your plugins
         { import = "plugins" },
     },
--- lua/plugins/mason-null-ls.lua
+++ lua/plugins/mason-null-ls.lua
@@ -1,14 +1,14 @@
 return {
-  "jay-babu/mason-null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "williamboman/mason.nvim",
-    "jose-elias-alvarez/null-ls.nvim",
-  },
-  config = function()
-    require("mason-null-ls").setup({
-      automatic_setup = true,
-    }) -- require your null-ls config here (example below)
-    -- require("mason-null-ls").setup_handlers()
-  end,
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "williamboman/mason.nvim",
+        "nvimtools/none-ls.nvim",
+    },
+    config = function()
+        require("mason-null-ls").setup({
+            automatic_setup = true,
+        }) -- require your null-ls config here (example below)
+        -- require("mason-null-ls").setup_handlers()
+    end,
 }
