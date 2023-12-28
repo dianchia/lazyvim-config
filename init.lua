a8ea1928b52e03748398eab909078f435dd50991
dianchia <dianrui520@hotmail.com>
2023-12-28 21:16:45 +0800

    Updated init.lua to include python host for linux

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require("config.lazy")
 
 -- vim.g.python3_host_prog = jit.os == "Windows" and os.getenv("LOCALAPPDATA") .. "/nvim/.venv/Scripts/python.exe"
 --     or "~/.config/nvim/.venv/bin/python"
-vim.g.python3_host_prog = vim.fn.stdpath("config") .. "/.venv/Scripts/python.exe"
+vim.g.python3_host_prog = jit.os == "Windows" and vim.fn.stdpath("config") .. "/.venv/Scripts/python.exe"
+    or vim.fn.stdpath("config") .. "/.venv/bin/python"
 vim.cmd.colorscheme("catppuccin")
 vim.opt.foldmethod = "marker"
