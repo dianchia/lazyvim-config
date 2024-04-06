1f435e7700d98fa254b4ba9dfdabc102ce9cfb86
dianchia <dianrui520@hotmail.com>
2024-04-06 22:30:54 +0800

    Changed nvim-silicon background color and num_separator

--- lua/plugins/nvim-silicon.lua
+++ lua/plugins/nvim-silicon.lua
@@ -8,13 +8,14 @@ return {
     opts = {
         font = "CaskaydiaCove Nerd Font Mono=34",
         theme = "Catppuccin-macchiato",
-        background = "#b7bdf8",
         line_offset = function(args)
             return args.line1
         end,
         language = function()
             return vim.bo.filetype
         end,
+        gobble = true,
+        num_separator = "\u{258f}",
         window_title = function()
             return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
         end,
