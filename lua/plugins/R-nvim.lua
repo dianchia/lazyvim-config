9cc910b1e07de07b81f5ec4e28317494cec008e1
dianchia <dianrui520@hotmail.com>
2024-10-30 10:21:20 +0800

    Use radian as the default R terminal

---
+++ lua/plugins/R-nvim.lua
@@ -0,0 +1,12 @@
+return {
+    "R-nvim/R.nvim",
+    opts = {
+        R_app = "radian",
+        R_cmd = "R",
+        R_path = "C:\\Users\\ruidian\\AppData\\Local\\LazyVim\\.venv\\Scripts\\",
+        auto_start = "always",
+        auto_quit = true,
+        bracketed_paste = true,
+        esc_term = true,
+    },
+}
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,25 +10,5 @@ return {
             shade = "dark",
             percentage = 0.15,
         },
-        integrations = {
-            bufferline = true,
-            leap = true,
-            mason = true,
-            mini = true,
-            neotree = true,
-            navic = {
-                enabled = true,
-                custom_bg = "NONE",
-            },
-            notify = true,
-            treesitter = true,
-            noice = true,
-            lsp_trouble = true,
-            illuminate = {
-                enabled = true,
-                lsp = true,
-            },
-            which_key = true,
-        },
     },
 }
